9f89be198d07813c38f82afc7a6172993eeb4fda
fengcancan <cancan.feng@yinyuetai.com>
2016-08-19 20:10:14 +0800

    工程初始化

---
+++ testjs/a.js
@@ -0,0 +1,14 @@
+var http = require("http");
+/*http.createServer(function(req, res) {
+	res.writeHead( 200 , {"Content-Type":"text/html"});
+	res.write("<h1>Node.js</h1>");
+	res.write("<p>Hello World</p>");
+	res.end("<p>beyondweb.cn</p>");
+}).listen(3000);
+console.log("HTTP server is listening at port 3000.");*/
+var fs =require('fs');
+http.createServer(function(req, res) {
+	res.writeHead(200,{'Content-Type':'image/png'});
+	fs.createReadStream('./image.png').pipe(res);
+}).listen(3000);
+console.log('Server running at http://localhost:3000')
